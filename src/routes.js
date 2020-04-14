d01a69604098045ee9a19426fdf3a2cd27e9d8e8
gfaraujo <guilherme.araujo@imd.ufrn.br>
2020-04-14 17:42:34 -0300

    Adicionando ScrollToTop
    Necessário para que a cada clique a página role para o topo

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import Epidemics from './pages/Epidemics';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function Routes() {
     return(
         <BrowserRouter basename="covid19">
-          
+            <ScrollToTop />
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/epidemics" component={Epidemics} />
